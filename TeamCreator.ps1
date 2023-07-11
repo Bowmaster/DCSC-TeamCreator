fe0598c723c60785e126971a47df06aaa6a4809d
Bowmaster <ben.lutterbach@gmail.com>
2023-07-11 17:00:04 -0400

    Various updates and improvements, particularly to error handling and gender'd teams

--- TeamCreator.ps1
+++ TeamCreator.ps1
@@ -2,38 +2,75 @@ param(
     [Parameter(Mandatory=$true)]
     [ValidateSet('U6','U8','U10','U12','U14')]
     [string]$Division,
+
     [Parameter(Mandatory=$true)]
     [int]$TeamCount,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Coed','Male','Female')]
+    [string]$ProcessGender,
+
     [Parameter(Mandatory=$true)]
     [ValidateScript({if (Test-Path $_) {
         $true
     }else{throw "Could not find file $_"}})]
     [string]$PlayerInputFile,
+
     [Parameter(Mandatory=$true)]
     [ValidateScript({if (Test-Path $_) {
         $true
     }else{throw "Could not find file $_"}})]
     [string]$CoachInputFile,
+
     [Parameter(Mandatory=$true)]
-    [string]$OutputFile
+    [ValidateScript({if (Test-Path $_ -Type Container) {
+        $true
+    }else{throw "Could not find directory $_ or $_ was not a directory but rather, a file"}})]
+    [string]$OutputDirectory,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$OutputObj
 )
 switch ($Division) {
     'U6' { $MaxPerDayPractices = 6 }
     'U8' { $MaxPerDayPractices = 2 }
-    'U10' { $MaxPerDayPractices = 2 }
+    'U10' { $MaxPerDayPractices = 3 }
     'U12' { $MaxPerDayPractices = 2 }
     'U14' { $MaxPerDayPractices = 1 }
     Default { throw "Incorrect value for Division ($Division) was supplied"}
 }
 $StartNum = 0
 $Teams = @()
-$Players = Get-Content -Path $PlayerInputFile | ConvertFrom-Csv
-$Players = $Players | sort -Property AgeInDays
 $MaxPlayers = [math]::Ceiling(($Players.Count / $TeamCount))
 $Coaches = Get-Content -Path $CoachInputFile | ConvertFrom-Csv
-$HeadCoaches = $Coaches | ? {$_.'Team Personnel Role' -eq 'Head Coach'}
-$HeadCoaches | ? {$_.'Preferred Practice Day' -eq 'No Answer'} | % {$_.'Preferred Practice Day' = 'XX_No_Answer'}
-$AssistantCoaches = $Coaches | ? {$_.'Team Personnel Role' -eq 'Assistant Coach'}
+$Players = Get-Content -Path $PlayerInputFile | ConvertFrom-Csv
+$Players = $Players | ? {$_.PlayerId -match '^[0-9]+$' -and [int]$_.PlayerId -ge 1000000}
+if (-not $Players) {
+   throw "No usable player data was found from $PlayerInputFile! Please check the file contents and try again."
+}
+if ($ProcessGender -eq "Coed") {
+    # Nothing to do, this is easy as we don't have to filter anything
+} elseif ($ProcessGender -eq "Male") {
+    $Players = $Players | ? {$_.Gender -eq 'M'}
+    $ignoredPlayers = $Players | ? {$_.Gender -eq 'F'}
+} elseif ($ProcessGender -eq "Female") {
+    $Players = $Players | ? {$_.Gender -eq 'F'}
+    $ignoredPlayers = $Players | ? {$_.Gender -eq 'M'}
+} else {
+    throw "Couldn't figure out how to handle Gender here..."
+}
+
+if (-not $Players) {
+    throw "Could not find any players after handling gender..."
+}
+
+$IneligibleCoaches = $Coaches | ? {$_.Email -in $ignoredPlayers.'Primary Contact Email' -or $_.Email -in $ignoredPlayers.'Secondary Contact Email'}
+$HeadCoaches = $Coaches | ? {$_.'Team Personnel Role' -eq 'Head Coach'} | ? {$_.VolunteerId -notin $IneligibleCoaches.VolunteerId}
+#$HeadCoaches | ? {$_.'Preferred Practice Day' -eq 'No Answer'} | % {$_.'Preferred Practice Day' = 'XX_No_Answer'}
+$AssistantCoaches = $Coaches | ? {$_.'Team Personnel Role' -eq 'Assistant Coach' -and $_.UserId -notin $HeadCoaches.UserId} | ? {$_.VolunteerId -notin $IneligibleCoaches.VolunteerId}
+Write-Host "HeadCoaches: $($HeadCoaches.'Team Personnel Name')"
+Write-Host "AssistantCoaches: $($AssistantCoaches.'Team Personnel Name')"
+timeout -1
 for ($i = 1; $i -le $TeamCount; $i++) {
     $obj = New-Object PSObject
     $obj | Add-Member -MemberType NoteProperty -Name HeadCoachId -Value @($HeadCoaches | ? {$_.VolunteerID -notin $Teams.HeadCoachId})[0].VolunteerId
@@ -43,6 +80,7 @@ for ($i = 1; $i -le $TeamCount; $i++) {
     $obj | Add-Member -MemberType NoteProperty -Name AssistantCoachName -Value $null
     $obj | Add-Member -MemberType NoteProperty -Name AssistantCoachEmail -Value $null
     $obj | Add-Member -MemberType NoteProperty -Name PracticeDay -Value $null
+    $obj | Add-Member -MemberType NoteProperty -Name PreferredPracticeTime -Value ($HeadCoaches | ? {$_.VolunteerID -eq $obj.HeadCoachId}).'Preferred Practice Time'
     $obj | Add-Member -MemberType NoteProperty -Name TeamName -Value "Team$(($i + $StartNum).ToString().PadLeft(2,'0'))"
     $obj | Add-Member -MemberType NoteProperty -Name Players -Value @()
     $obj | Add-Member -MemberType ScriptProperty -Name PlayerNames -Value {$this.Players.PlayerFullName | Sort-Object}
@@ -56,16 +94,23 @@ for ($i = 1; $i -le $TeamCount; $i++) {
     $obj.AssistantCoachName = ($AssistantCoaches | ? {$_.VolunteerID -eq $obj.AssistantCoachId}).'Team Personnel Name'
     $obj.AssistantCoachEmail = ($AssistantCoaches | ? {$_.VolunteerID -eq $obj.AssistantCoachId}).Email
     $Teams += $obj
+    if ($obj.HeadCoachId) {
+        $obj.TeamName = "$($obj.TeamName)-$($obj.HeadCoachName.Split(' ')[-1])$(if($obj.AssistantCoachName){"/$($obj.AssistantCoachName.Split(' ')[-1])"})"
+    }
 }
 
 foreach ($team in ($Teams)) {
     $row = $HeadCoaches | ? {$team.HeadCoachId -eq $_.VolunteerId}
-    switch -wildcard ($row.'Preferred Practice Day'.ToLower()) {
-        "mon*" { $eligibleCoachPractice = @('Monday') }
-        "tues*" { $eligibleCoachPractice = @('Tuesday') }
-        "wed*" { $eligibleCoachPractice = @('Wednesday') }
-        "thur*" { $eligibleCoachPractice = @('Thursday') }
-        Default { $eligibleCoachPractice = @('Monday','Tuesday','Wednesday','Thursday') }
+    if ($row) {
+        switch -wildcard ($row.'Preferred Practice Day'.ToLower()) {
+            "mon*" { $eligibleCoachPractice = @('Monday') }
+            "tues*" { $eligibleCoachPractice = @('Tuesday') }
+            "wed*" { $eligibleCoachPractice = @('Wednesday') }
+            "thur*" { $eligibleCoachPractice = @('Thursday') }
+            Default { $eligibleCoachPractice = @('Monday','Tuesday','Wednesday','Thursday') }
+        }
+    } else {
+        $eligibleCoachPractice = @('Monday','Tuesday','Wednesday','Thursday')
     }
     foreach ($day in $eligibleCoachPractice) {
         if (($Teams.PracticeDay | ? {$_ -eq $day}).Count -le $MaxPerDayPractices) {
@@ -78,12 +123,14 @@ foreach ($team in ($Teams)) {
 }
 
 $Players | % {
+    $playerObj = $_
     $_ | Add-Member -MemberType NoteProperty -Name ParentFullName -Value "$($_.'Parent FirstName') $($_.'Parent LastName')" -Force
     if ([string]::IsNullOrWhiteSpace($_.'Secondary Contact FirstName') -eq $false -and $_.'Secondary Contact FirstName' -ne 'No Answer') {
         $_ | Add-Member -MemberType NoteProperty -Name SecondaryContactFullName -Value "$($_.'Secondary Contact FirstName') $($_.'Secondary Contact LastName')" -Force
     }
     $_ | Add-Member -MemberType NoteProperty -Name PlayerFullName -Value $_.'Player Name' -Force
-    $_ | Add-Member -MemberType NoteProperty -Name AgeInDays -Value ((Get-Date) - [datetime]$_.'Date Of Birth').Days -Force
+    Write-Verbose "Processing $($_.PlayerFullName), $($_.'Date Of Birth')"
+    $_ | Add-Member -MemberType NoteProperty -Name AgeInDays -Value ((Get-Date) - [datetime]($_.'Date Of Birth')).Days -Force
     $_ | Add-Member -MemberType NoteProperty -Name NewTeamName -Value $null -Force
     $returningProp = ($_.PSObject.Properties | ? {$_.Name.StartsWith('New or Returning')}).Name
     if ($_.'Recent Team' -ne 'NA' -or $_.$returningProp -eq 'Returning') {
@@ -93,17 +140,34 @@ $Players | % {
     }
     $practiceProp = ($_.PSObject.Properties | ? {$_.Name.StartsWith('Days you CANNOT Practice')}).Name
     $_ | Add-Member -MemberType NoteProperty -Name EligiblePracticeDays -Value @('Monday','Tuesday','Wednesday','Thursday')
-    $noPracticeDays = $_.$practiceProp.Split(',')
-    foreach ($day in $noPracticeDays) {
-        if ($day -eq 'No Answer') {
-            # Any day is ok
-            continue
+    if ([string]::IsNullOrWhiteSpace($practiceProp)) {
+        Write-Warning "No Practice Day property found for Player $($_.'Player Name')"
+        $noPracticeDays = 'No Answer'
+    } else {
+        if (-not $_.$practiceProp) {
+            Write-Host "Player $($playerObj.'Player Name') is showing an empty value for Practice day field '$practiceProp'" -ForegroundColor Red
+            Write-Host "Please verify their practice availability manually" -ForegroundColor Red
+            $noPracticeDays = 'No Answer'
         } else {
-            $_.EligiblePracticeDays = $_.EligiblePracticeDays | ? {$_ -ne $day}
+            try {
+                $noPracticeDays = $_.$practiceProp.Split(',')
+            }
+            catch {
+                Write-Host "Unable to determine the eligible Practice Days for player $($playerObj.'Player Name'), please verify their practice day manually!!" -ForegroundColor Red
+                $noPracticeDays = 'No Answer'
+            }
+        }
+        foreach ($day in $noPracticeDays) {
+            if ($day -eq 'No Answer') {
+                # Any day is ok
+                continue
+            } else {
+                $_.EligiblePracticeDays = $_.EligiblePracticeDays | ? {$_ -ne $day}
+            }
         }
     }
 }
-
+$Players = $Players | Sort-Object -Property AgeInDays
 for ($i = 0; $i -lt $Players.Count; $i++) {
     if ($Players[$i].NewTeamName) {
         Write-Warning "Skipping player $($Players[$i].PlayerFullName) as they are already assigned to $($Players[$i].NewTeamName)"
@@ -215,8 +279,9 @@ foreach ($team in $Teams) {
         }
     }
 }
-#$Output
-#$Teams
-#$Players
 
-$Output | ConvertTo-Csv | Out-File $OutputFile -Force
+$Output | ConvertTo-Csv | Out-File $(Join-Path $OutputDirectory "$Division-$ProcessGender-TeamData-$((Get-Date).ToFileTime()).csv") -Force
+if ($OutputObj) {
+    $Var = New-Variable -Name "DCSC$($Division)Team" -Value $Teams -PassThru -Force
+    $Var.Value
+}
